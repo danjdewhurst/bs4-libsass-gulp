79519cc27743db1d618d28a57526b5808ff6cda4
Daniel Dewhurst <danjdewhurst@gmail.com>
2017-04-02 23:13:19 +0100

    Initial commit
    Signed-off-by: Daniel Dewhurst <danjdewhurst@gmail.com>

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/style.scss')
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public_html/assets/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass']);
