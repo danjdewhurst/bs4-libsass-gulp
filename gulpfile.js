7fcf279b5643ac6fbde809fde1025ef92915a16e
Daniel Dewhurst <daniel.dewhurst@reckless.agency>
2017-04-06 09:23:34 +0100

    Added postcss-flexbugs-fixes to SASS compiling process as recommended by BS4 Changed autoprefixer to add vendor prefixes for browsers with a global usage of over 5%
    Signed-off-by: Daniel Dewhurst <daniel.dewhurst@reckless.agency>

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
+var flexbugs = require('postcss-flexbugs-fixes');
 var autoprefixer = require('autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('./src/sass/style.scss')
-    .pipe(postcss([ autoprefixer() ]))
+    .pipe(postcss([
+      flexbugs(),
+      autoprefixer({
+        browsers: ['> 5%']
+      })
+    ]))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public_html/assets/css'));
 });
