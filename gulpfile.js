6e336e80df65e82f0b3c4c72a3ed1e33fc3f853f
Daniel Dewhurst <danjdewhurst@users.noreply.github.com>
2017-04-11 11:51:52 +0100

    Updated gulpfile.js to include flexibility polyfill

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var flexbugs = require('postcss-flexbugs-fixes');
+var flexibility = require('postcss-flexibility');
 var autoprefixer = require('autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('./src/sass/style.scss')
     .pipe(postcss([
       flexbugs(),
+      flexibility(),
       autoprefixer({
-        browsers: ['> 5%']
+        browsers: ['> 1%']
       })
     ]))
     .pipe(sass().on('error', sass.logError))
